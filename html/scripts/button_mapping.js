638f5aefa1e991f1e556777334953ad36058612b
Andrei Gasparovici <andreigasparovici1@gmail.com>
2017-03-30 16:31:15 +0300

    Created Docker-based compilation and debugging

---
+++ html/scripts/button_mapping.js
@@ -0,0 +1,49 @@
+var breakpointList = [];
+var makers = {};
+var debuggerLine;
+var crtLineMarker;
+
+$("#compile_code_button").click(function() {
+    var code = editor.getValue();
+    socket.emit("code",code);
+});
+$("#run_code_button").click(function() {
+    sendCommand("run");
+});
+$("#debug_code_button").click(function() {
+    if (breakpointList.length > 0)
+        sendCommand("run");
+    else {
+        addBreakpoint();
+        sendCommand("run");
+    }
+})
+$("#add_breakpoint_button").click(function() {
+    var pos = editor.getCursorPosition();
+    var crtRow = parseInt(pos.row)+1;
+    toggleBreakpoint(crtRow-1);
+})
+$("#step_debugger").click(function() {
+    stepDebugger();
+});
+
+let temp = 2;
+
+function stepDebugger() {
+    sendCommand("s");
+    moveHighlight(temp);
+    temp = temp+1;
+}
+
+var Range = ace.require('ace/range').Range;
+
+function moveHighlight(row) {
+    if (crtLineMarker != null) {
+        editor.getSession().removeMarker(crtLineMarker);
+    }
+    crtLineMarker = editor.getSession().addMarker(new Range(row, 0, row, 1), "myMarker", "fullLine");
+}
+
+function highLine(row) {
+    marker = editor.getSession().addMarker(new Range(row, 0, row, 1),"myMarker","fullLine");
+}
---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+const socketIoClient = require('socket.io-client');
+const childProcess = require('child_process');
+
+let Docker = require('dockerode');
+let Chance = require('chance');
+
+let app = express();
+let httpServer = http.Server(app);
+let io = socketIo(httpServer);
+
+let chance = new Chance();
+
+let docker = new Docker();
+
+app.use(require('express').static(path.join(__dirname,"html")));
+
+let CONTAINERS = {};
+let USED_PORTS = {};
+
+function getAvailablePort(){
+    let port = chance.integer({min: 2000, max: 2500});
+    while(USED_PORTS[port.toString()]){
+        port = chance.integer({min: 2000, max: 2500});
+    }
+    return port;
+}
+
+io.on('connection', (socket)=>{
+    console.log('A user connected with id ',socket.id);
+
+    let port = getAvailablePort();
+
+    docker.createContainer({
+        Image: 'learning-pointers', 
+        name: socket.id.toString()+'-container',
+        ExposedPorts: {'3001/tcp': {} },
+        PortBindings: {'3001/tcp': [{ 'HostPort': port.toString() }] },
+        Privileged: true
+    }, 
+        (err, container) => {
+
+            if(err) throw err;
+
+            container.start((err, data) =>{
+                
+                USED_PORTS[port.toString()] = true;
+
+                CONTAINERS[socket.id] = socketIoClient('http://localhost:'+port.toString());
+                
+                CONTAINERS[socket.id].on('connect',()=>{
+                    console.log('Connected to container!');
+                });
+
+                socket.on('code',(data)=>{
+                    CONTAINERS[socket.id].emit('code',data);
+                });
+
+                CONTAINERS[socket.id].on('compile_error',(data)=>{
+                    socket.emit('compile_error',data);
+                });
+
+                CONTAINERS[socket.id].on('compile_success',(data)=>{
+                    socket.emit('compile_error',data);
+                });
+
+                CONTAINERS[socket.id].on('gdb_stdout', (data)=>{
+                    socket.emit('gdb_stdout',data);
+                });
+
+                CONTAINERS[socket.id].on('gdb_stderr', (data)=>{
+                    socket.emit('gdb_stderr',data);
+                });
+
+                socket.on('request_expressions',(data)=>{
+                    CONTAINERS[socket.id].emit('request_expressions',data);
+                });
+
+                CONTAINERS[socket.id].on('request_expressions_response',(data)=>{
+                    socket.emit('request_expressions_response',data);
+                });
+
+                socket.on('gdb_cmd',(data)=>{
+                    CONTAINERS[socket.id].emit('gdb_cmd',data);
+                });
+
+                socket.on('disconnect',(data)=>{
+                    USED_PORTS[port.toString()] = false;
+                    container.stop().then(function(container) {
+                        return container.remove();
+                    }).then(function(data) {
+                        console.log('Container removed');
+                    }).catch(function(err) {
+                        console.log(err);
+                    });
+                });
+            });
+    });
+});
+
+app.get('/',(req,res)=>{
+	res.sendFile(path.join(__dirname,"html","index.html"));
+});
+
+httpServer.listen(3000,()=>{
+	console.log('Server started on port %s',3000);
+});
+process.stdin.on('data',(data)=>{
+
+    if(data.toString() == 'remove\n'){
+
+        docker.listContainers((err, containers) => {
+            containers.forEach((containerInfo) => {
+                console.log('Stopping container');
+                docker.getContainer(containerInfo.Id).stop(function(){
+                    console.log('Stopped');
+                    console.log('Removing container');
+                    docker.getContainer(containerInfo.Id).remove(()=>{
+                        console.log('Removed');
+                    });
+                });
+            });
+        });
+    }
+});
