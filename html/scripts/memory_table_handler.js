d4ae474888c8e3990446940896f3d62245e9779f
Timi <timitoc@yahoo.com>
2017-04-27 22:55:03 +0300

    Added variables tables for memory mapping

--- html/scripts/memory_table_handler.js
+++ html/scripts/memory_table_handler.js
@@ -1,38 +1,57 @@
-var memoryHandler = function() {
-    var UIElement;
-    var n, m;
-    var init = function(JUIElement) {
-        UIElement = JUIElement;
-    }
+var MemoryHandler = function() {
+    this.UIElement=null;
+    this.JUIElement=null;
+    this.n = 10;
+    this.m = 10;
+}
 
-    var resize = function(height, width) {
-        n = height;
-        m = width;
-        invalidate();    
-    }
+MemoryHandler.prototype.init = function(UIElement) {
+    this.UIElement = UIElement;
+    this.JUIElement = $(this.UIElement);
+    this.invalidate();
+}
 
-    var invalidate = function() {
-        UIElement.style.width  = '210px';
-        UIElement.style.border = '1px solid black';
-        UIElement.empty();
-        for(var i = 0; i < n; i++){
-            var tr = UIElement.insertRow();
-            for(var j = 0; j < m; j++){
-                var td = tr.insertCell();
-                td.addClass("memory_cell");
-                td.appendChild(document.createTextNode(i*n + j));
-                td.style.border = '1px solid black';
-                td.style.width = '30px';
-            }
+MemoryHandler.prototype.resize = function(height, width) {
+    this.n = height;
+    this.m = width;
+    this.invalidate();    
+}
+
+MemoryHandler.prototype.invalidate = function() {
+    //this.JUIElement.empty();
+    this.UIElement.style.width  = '210px';
+    this.UIElement.style.border = '1px solid black';
+    for(var i = 0; i < this.n; i++){
+        var tr = this.UIElement.insertRow();
+        for(var j = 0; j < this.m; j++){
+            var td = tr.insertCell();
+            //$(td).addClass("memory_cell");
+            td.appendChild(document.createTextNode(i*this.n + j));
+            td.style.border = '1px solid black';
+            td.style.width = '30px';
         }
     }
-
-    var colorRange = function(lo, hi) {
-        var filteredCells = $('memory_cell').filter(function(i){
-            var num = parseInt(this.text, 10);
-            return (num >= lo && num <= hi)
-        });
-        filteredCells.css('color', 'red');
-        
-    }
 }
+
+MemoryHandler.prototype.colorRange = function(lo, hi) {
+    var filteredCells = $('memory_cell').filter(function(i){
+        var num = parseInt(this.text, 10);
+        return (num >= lo && num <= hi)
+    });
+    filteredCells.css('color', 'red');
+}
+
+var memoryHandler = new MemoryHandler();
+
+function initLeftPart() {
+    $("#left_content").text("Memory status");
+    function tableCreate(){
+        var body = document.getElementById("left_content"),
+            tbl  = document.createElement('table');
+        $(tbl).addClass("memory_table_class");
+        memoryHandler.init(tbl);
+        body.appendChild(tbl);
+    }
+    tableCreate();
+}
+initLeftPart();
---
+++ html/scripts/memory_var_x.js
@@ -0,0 +1,125 @@
+var MemoryVarHandler = function() {
+    this.JUIElement = null;
+    this.createButton = null;
+}
+
+
+MemoryVarHandler.prototype.init = function(wrapper) {
+    this.JUIElement = jQuery('<div/>', {
+            class: 'MVHtree'
+    });
+    this.createButton = jQuery('<button/>', {
+            class: 'MVHButton',
+            width: 50,
+            height: 50
+    });
+    wrapper.append(this.JUIElement);
+    this.addRow();
+    wrapper.append(this.createButton);
+}
+
+MemoryVarHandler.prototype.addRow = function() {
+
+    var self = this;
+    $(document).ready(function(){
+        // tree data
+        var data = [];
+        
+        element = self.JUIElement.jstree({
+            plugins: ["table", "contextmenu", "types"],
+            contextmenu: {         
+                "items": function($node) {
+                    var tree = self.JUIElement.jstree(true);
+                    return {
+                        "Create": {
+                            "separator_before": false,
+                            "separator_after": false,
+                            "label": "Create",
+                            "action": function (obj) { 
+                                // $node = tree.create_node();
+                                // tree.edit($node);
+                                self.JUIElement.jstree("create_node", null, null, "last", function (node) {
+                                    this.edit(node);
+                                    this.hide_icons();
+                                });
+                            }
+                        },
+                        "Rename": {
+                            "separator_before": false,
+                            "separator_after": false,
+                            "label": "Rename",
+                            "action": function (obj) { 
+                                tree.edit($node);
+                            }
+                        },                         
+                        "Remove": {
+                            "separator_before": false,
+                            "separator_after": false,
+                            "label": "Remove",
+                            "action": function (obj) { 
+                                tree.delete_node($node);
+                            }
+                        }
+                    };
+                }
+            },
+            core: {
+                check_callback: true,
+                data: data
+            },
+            // configure tree table
+            table: {
+                columns: [
+                    {width: 100, header: "Simple"}
+                ],
+                resizable: true,
+                draggable: false,
+                contextmenu: true,
+                width: 100,
+                height: 300
+            },
+        });
+        
+        self.JUIElement.bind(
+            "select_node.jstree", function(evt, data){
+                var newText = "Some new text";
+                var inst = $.jstree.reference(data.node);
+                inst.edit(data.node);
+            }
+        );
+        self.JUIElement.bind(
+            "rename_node.jstree", function(evt, data){
+                console.log(JSON.stringify(data.node));
+                if (data.node.original.smec == true || 
+                    data.node.smec === true) {
+                    self.JUIElement.jstree("create_node", null, {text: "", smec: true}, "last", function (node) {
+                        console.log(JSON.stringify(node));
+                        this.hide_icons();
+                    });
+                }
+                data.node.smec = false;
+                data.node.original.smec = false;
+            }
+        );
+        self.JUIElement.jstree("create_node", null, {text: "", smec: true}, "last", function (node) {
+            console.log(JSON.stringify(node));
+            console.log(JSON.stringify(self.JUIElement.jstree(true).get_node(node.id)));
+            this.hide_icons();
+        });
+    });
+}
+
+var simpleVar = new MemoryVarHandler();
+var pointerVar = new MemoryVarHandler();
+
+wrapper1 = jQuery('<div/>');
+wrapper2 = jQuery('<div/>');
+
+wrapper1.css('display', 'inline-block');
+wrapper2.css('display', 'inline-block');
+
+simpleVar.init(wrapper1);
+pointerVar.init(wrapper2);
+
+$("#left_content").append(wrapper1);
+$("#left_content").append(wrapper2);
