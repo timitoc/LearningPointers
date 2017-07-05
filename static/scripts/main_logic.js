2031feed27127310cc0545bb4c48b372ad422b10
Timi <timitoc@yahoo.com>
2017-07-05 22:22:31 +0300

    Fixed deleting breakpoints problem

--- static/scripts/main_logic.js
+++ static/scripts/main_logic.js
@@ -36,3 +36,20 @@ $( document ).ready(function() {
     console.log(JSON.stringify(result));
 });
 
+//https://stackoverflow.com/questions/1068834/object-comparison-in-javascript
+Object.equals = function( x, y ) {
+  if ( x === y ) return true;
+  if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
+  if ( x.constructor !== y.constructor ) return false;
+  for ( var p in x ) {
+    if ( ! x.hasOwnProperty( p ) ) continue;
+    if ( ! y.hasOwnProperty( p ) ) return false;
+    if ( x[ p ] === y[ p ] ) continue;
+    if ( typeof( x[ p ] ) !== "object" ) return false;
+    if ( ! Object.equals( x[ p ],  y[ p ] ) ) return false;
+  }
+  for ( p in y ) {
+    if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+  }
+  return true;
+}
