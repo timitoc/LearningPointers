db57b73ba96cda03e9cc498baef878cea206f245
Timi <timitoc@yahoo.com>
2017-07-12 18:02:53 +0300

    Added breakpoint options structure

--- static/scripts/editor_gutter_setup.js
+++ static/scripts/editor_gutter_setup.js
@@ -1,6 +1,8 @@
 function setGutterInteractions() {
     console.log("Setting gutter interactions");
-    var marker, range
+
+    // Inspired by https://groups.google.com/forum/#!topic/ace-discuss/mRIXzXwYrnk
+    var bracketMarker, range
     var gutter = editor.renderer.$gutterLayer
     editor.on("guttermousemove", function(e) {
         var gutterRegion = gutter.getRegion(e);
@@ -10,26 +12,44 @@ function setGutterInteractions() {
                 var session = editor.session;
                 range = session.getParentFoldRangeData(row).range || session.getFoldWidgetRange(row);
             }
-        } else {
-            range = null        
+        } 
+        else if (gutterRegion == "markers") {
+            var position = e.getDocumentPosition();
+            var left = (e.clientX+2) + "px"; 
+            var top = (e.clientY+2) + "px"; 
+            var divid = "breakpoint_options";
+            $("#"+divid).css('left',left); 
+            $("#"+divid).css('top',top); 
+            //$("#"+divid).toggle(); 
+            range = null;       
         }
-        if (marker) { editor.session.removeMarker(marker); }    
-        marker = range && editor.session.addMarker(range, "ace_bracket", "fullLine");
-    })
+        else
+            range = null;
+        if (bracketMarker) { editor.session.removeMarker(bracketMarker); }    
+        bracketMarker = range && editor.session.addMarker(range, "ace_bracket", "fullLine");
+    });
+
+    editor.renderer.$gutter.addEventListener("mouseout", function() {
+        editor.session.removeMarker(bracketMarker); 
+        bracketMarker = null
+    });
 
     editor.on("guttermousedown", function (e) {
         var row = e.getDocumentPosition().row;
         var gutterRegion = editor.renderer.$gutterLayer.getRegion(e);
         if (gutterRegion == "foldWidgets") {
-            console.log("Folding");
+            //console.log("Folding");
         }
         else {
             toggleBreakpoint(row);
         }
     });
 
-    editor.renderer.$gutter.addEventListener("mouseout", function() {
-        editor.session.removeMarker(marker); 
-        marker = null
-    });
 }
+
+
+
+/// Breakpoint options
+
+var isBROShown;
+
