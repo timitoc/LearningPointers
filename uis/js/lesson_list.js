8dcb89c08ee5ead3033a23db331ba5654ae5ae4f
Andrei Gasparovici <andreigasparovici1@gmail.com>
2017-07-15 10:54:21 +0300

    Added ui for lesson list

---
+++ uis/js/lesson_list.js
@@ -0,0 +1,22 @@
+$(function() {
+	var ratings = document.getElementsByClassName("lesson-rating");
+	for(var i = 0; i < ratings.length; i++) {
+		var ratingProperty = ratings[i].getAttribute("rating");
+		var ratingValue = parseInt(ratingProperty.split('/')[0])
+		var ratingTotal = parseInt(ratingProperty.split('/')[1])
+
+		var j = 0;
+
+		for(; j < ratingValue; j++ ) {
+			var starImage = document.createElement("img");
+			starImage.src = "img/star.svg";
+			ratings[i].appendChild(starImage);
+		}
+
+		for(; j < ratingTotal; j++) {
+			var starImage = document.createElement("img");
+			starImage.src = "img/stargray.svg";
+			ratings[i].appendChild(starImage);
+		}
+	}
+});
